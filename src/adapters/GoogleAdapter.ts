4fa239ab8b2f4cdcb16ead3372360a6748027f9a
Taylor Romero <me@taylorrome.ro>
2023-03-07 10:38:44 -0700

    patch: type fix

--- src/adapters/GoogleAdapter.ts
+++ src/adapters/GoogleAdapter.ts
@@ -6,7 +6,8 @@ import {
 } from '../sheetsReporter.types'
 
 export default class SheetsReporterGoogleAdapter
-	implements IGoogleSheetsAdapter {
+	implements IGoogleSheetsAdapter
+{
 	private serviceEmail: string
 	private privateKey: string
 	private spreadsheetInstancesById: Record<string, Promise<GoogleSpreadsheet>> =
